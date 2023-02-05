40c04ad59886c47ea7e3c4f32c5128eaff370a3d
Juan Diego Flores <flores.juan@correo.unimet.edu.ve>
2023-02-05 03:31:11 -0400

    Bar chart skills

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
-function isElementInViewport(el) {
-    var rect = el.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
+const skillsJson = {
+    'HTML': "300px",
+    'CSS' : "250px",
+    'Javascript' : "200px",
+    'Java' : "200px",
+    'Python' : "200px",
+}
 
-  const chartWrapper = document.querySelector(".chart-wrapper");
-window.addEventListener("scroll", scrollHandler);
-function scrollHandler() {
-  if (isElementInViewport(chartWrapper)) chartWrapper.classList.add("in-view");
+let chart = document.getElementById("skills-chart")
+
+createChart(chart)
+
+function createChart(chart){
+
+    let temp;
+    let j=0;
+    const claves = Object.keys(skillsJson);
+
+    for (let i in skillsJson) {
+        temp = document.createElement("p");
+        temp.className = "skills-yaxis";
+        temp.innerText = claves[j]
+        chart.appendChild(temp)
+
+        temp = document.createElement("span");
+        temp.className = "skills-bar";
+        temp.style.width = skillsJson[claves[j]]
+        console.log(temp)
+        chart.appendChild(temp)
+
+        j++
+    }
 }
